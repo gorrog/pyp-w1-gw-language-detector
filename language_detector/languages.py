ec5680bc41977ce3d9c7df46e9f9cab9ed4e9a12
Will N <therewillbnoland@gmail.com>
2016-07-08 20:34:43 +0000

    Tests passing, solution appears to be working!

--- language_detector/languages.py
+++ language_detector/languages.py
@@ -3,7 +3,37 @@
 LANGUAGES = [
     {
         'name': 'Spanish',
-        'common_words': ['yo', 'tu', 'el', 'nosotros']  # add your words here
+        'common_words': ['el', 'la', 'de', 'que', 'y', 'a', 'en', 'un', 'ser', 'se',
+                    'no', 'haber', 'por', 'con', 'su', 'para', 'como', 'estar',
+                    'tener', 'le', 'lo', 'lo', 'todo', 'pero', 'más', 'hacer',
+                    'o', 'poder', 'decir', 'este', 'ir', 'otro', 'ese', 'la',
+                    'si', 'me', 'ya', 'ver', 'porque', 'dar', 'cuando', 'él',
+                    'muy', 'sin', 'vez', 'mucho', 'saber', 'qué', 'sobre',
+                    'mi', 'alguno', 'mismo', 'yo', 'también', 'hasta']
     },
-    #  include your extra languages here
+            {
+                'name': 'German',
+                'common_words': [
+                    'das', 'ist', 'du', 'ich', 'nicht', 'die', 'es', 'und',
+                    'der', 'was', 'wir', 'zu', 'ein', 'er', 'in', 'sie', 'mir',
+                    'mit', 'ja', 'wie', 'den', 'auf', 'mich', 'dass', 'so',
+                    'hier', 'eine', 'wenn', 'hat', 'all', 'sind', 'von',
+                    'dich', 'war', 'haben', 'für', 'an', 'habe', 'da', 'nein',
+                    'bin', 'noch', 'dir', 'uns', 'sich', 'nur',
+                    'einen', 'kann', 'dem'
+                ]
+            },
+            {
+                'name':'English',
+                'common_words': [
+                    'the', 'a', 'an', 'with', 'without', 'of', 'it', 'or',
+                    'nor','no', 'yes', 'know', 'my', 'very', 'already', 'this',
+                    'that', 'much', 'very', 'same', 'I', 'me', 'you', 'your',
+                    'what', 'when', 'also', 'always', 'why', 'he', 'she',
+                    'every', 'but', 'already', 'we', 'them', 'us', 'they',
+                    'will', 'do', 'have', 'had', 'has', 'been', 'in', 'go',
+                    'know', 'more' 
+                    
+                    ]
+            }
 ]
--- language_detector/main.py
+++ language_detector/main.py
@@ -2,8 +2,50 @@
 
 from .languages import LANGUAGES
 
-
 def detect_language(text, languages=LANGUAGES):
     """Returns the detected language of given text."""
     # implement your solution here
-    pass
+    '''makes the paragraph break into a list'''
+    word_list = text.split()
+
+    # How many languages do we have?
+    lang_count = len(LANGUAGES) 
+    
+    # We will now create a dictionary. Key name will be
+    # language name, and value (initially 0) will be the count of words
+    # in word_list that were found in our paragraph
+    lang_list = {}
+    for l in LANGUAGES:
+        tmp_lang = l["name"]
+        lang_list[tmp_lang] = 0 #Eg: lang_list["Chinese"] = 0
+        
+    # OK, now we have initialised our dictionary, it's time to count...
+    '''this will take each word from the word_list and test it with our group
+    of words'''
+    for x in word_list:
+    
+        '''this will take the find out how many languages we have and put them 
+        in a range'''
+        for y in range(lang_count):
+            
+            '''Stands for current language:''' 
+            current_lang = LANGUAGES[y]
+            
+            '''calls in the external list lang_list'''
+            #global lang_list
+          
+            '''this if staement makes a loop passing the word into the groups 
+            of languages'''
+            if x in current_lang['common_words']:
+                
+                '''once it matches they add the count of the of the language'''
+                lang_list[current_lang['name']] += 1 # Eg: lang_list["Spanish"] += 1
+                
+    # Now we have a list of languages and a count of the number of times their
+    # common word were found in our lang_list dictionary
+    
+    # Time to figure out which language has the highest value
+    ranking = sorted(lang_list, key=lang_list.get, reverse=True)
+    # We should now have a list of ranked languages
+    
+    return ranking[0]
